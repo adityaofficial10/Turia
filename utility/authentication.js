69ed505ee1adeaebffd28a1f3c0fa5c06d74f068
Aditya R Rudra <adityaberasia@gmail.com>
2021-04-24 02:01:51 +0530

    Methods for authentication added..

---
+++ utility/authentication.js
@@ -0,0 +1,23 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const {read, write} = require('../data/fetch');
+const { AuthenticationError } = require('apollo-server');
+
+async function login (email, password) {
+
+  const values = await read();
+  var ok = Array.isArray(values) ? values.findIndex(x => x.email == email) : 0;
+  if(ok) {
+      if(bcrypt.compareSync(password, values[ok].password)){
+          return values[ok];
+      }
+      else
+      return new AuthenticationError('Incorrect Password!');
+  }
+  else
+  return new AuthenticationError('There is no user registered with this email.');
+};
+
+module.exports = {
+    login
+};
