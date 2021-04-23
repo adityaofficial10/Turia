412038b15ac25e1c79e61ee74a621ba5dfd9a689
Aditya R Rudra <adityaberasia@gmail.com>
2021-04-24 02:00:50 +0530

    Server configuration added.

---
+++ server.js
@@ -0,0 +1,18 @@
+const { ApolloServer } = require('apollo-server')
+const typeDefs = require('./graphql/schema')
+const resolvers = require('./graphql/resolvers')
+const fetch = require('./data/fetch');
+const auth = require('./utility/authentication');
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: { fetch, auth }
+});
+  
+server
+    .listen()
+    .then(({ url }) => console.log('Server is running on localhost:4000'));
+  
+
+
