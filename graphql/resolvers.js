e5b344a8ef243da1ca80defcc3630ba6a73daa30
Aditya R Rudra <adityaberasia@gmail.com>
2021-04-24 12:42:59 +0530

    Resolvers for auth mutations added..

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -8,10 +8,13 @@ const resolvers = {
     },
     Mutation: {
         async login (root, {  email, password }, { auth }) {
-            await auth.login(email, password); 
+            const data = await auth.login(email, password); 
+            return data;
         },
         async create (root, { first_name, last_name, phone_number, email, password }, { fetch }) {
-            await fetch.write({first_name:first_name, last_name: last_name, phone_number: phone_number, email: email, password: password, coachmark_visited: '0', assessment_complete: '0', assessment_skipped: '0', created_date: (new Date()).toISOString(),last_login: '', last_updated_date: (new Date()).toISOString()});
+            const data = await fetch.write({first_name:first_name, last_name: last_name, phone_number: phone_number, email: email, password: password, coachmark_visited: '0', assessment_complete: '0', assessment_skipped: '0', created_date: (new Date()).toISOString(),last_login: '', last_updated_date: (new Date()).toISOString()});
+            console.log(data);
+            return data;
         }
     }
 
