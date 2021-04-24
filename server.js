80b16dcffbdfacb4bb70a5b140a79e2a552a60f8
Aditya R Rudra <adityaberasia@gmail.com>
2021-04-24 12:43:15 +0530

    Server updated.

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const server = new ApolloServer({
   
 server
     .listen()
-    .then(({ url }) => console.log('Server is running on localhost:4000'));
+    .then(({ url }) => console.log(`Server is running on ${url}`));
   
 
 
