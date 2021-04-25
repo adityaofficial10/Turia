66ac62084755e6de6718d9d83f72bd8dcbda9dfc
Aditya R Rudra <adityaberasia@gmail.com>
2021-04-26 00:14:03 +0530

    Auth functions added..

--- utility/authentication.js
+++ utility/authentication.js
@@ -1,22 +1,27 @@
+require('dotenv').config({
+    path:'.env'
+});
+
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const {read, write} = require('../data/fetch');
+const { read, write } = require('../data/fetch');
 const { AuthenticationError } = require('apollo-server');
+const cookieParser = require('cookie-parser');
 
-async function login (email, password) {
+async function login(email, password) {
 
-  const values = await read();
-  console.log(values);
-  var ok = Array.isArray(values) ? values.findIndex(x => x.email == email) : 0;
-  if(ok != -1) {
-      if(bcrypt.compareSync(password, values[ok].password)){
-          return values[ok];
-      }
-      else
-      return new AuthenticationError('Incorrect Password!');
-  }
-  else
-  return new AuthenticationError('There is no user registered with this email.');
+    const values = await read();
+    var ok = Array.isArray(values) ? values.findIndex(x => x.email == email) : 0;
+    if (ok != -1) {
+        if (bcrypt.compareSync(password, values[ok].password)) {
+            const token = jwt.sign({ id: values[ok].user_id }, process.env.SECRET_KEY, { expiresIn: '1h' });
+            return { user_id: values[ok].user_id, email: values[ok].email, token: token };
+        }
+        else
+            return new AuthenticationError('Incorrect Password!');
+    }
+    else
+        return new AuthenticationError('There is no user registered with this email.');
 };
 
 module.exports = {
